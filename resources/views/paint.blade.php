d94e653ac1ceb04478c2be4646bb6385850f9de0
tuan <tuemzz1@gmail.com>
2019-06-26 06:00:04 +0700

    add cipher key

--- resources/views/paint.blade.php
+++ resources/views/paint.blade.php
@@ -7,9 +7,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Document</title>
-    <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">
-    <script src="{{ URL::asset('js/jquery-3.4.1.min.js') }}"></script>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <script src="js/jquery-3.4.1.min.js"></script>
     <!--<link rel="stylesheet" href="./public/css/fontawesome.min.css" type='text/css'>-->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 </head>
